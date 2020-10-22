30f8babfbedb1e012cfa09371c1703bfe19bc111
gabriel restrepo <gabriel.restrepoi@udea.edu.co>
2020-10-22 08:31:25 -0500

    creado nuevo repositorio

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*Reciba una cadena de caracteres y separe los números del resto de caracteres, generando
+una cadena con solo caracteres y otra con los números que había en la cadena original. A
+partir de la cadena de números obtenida, el programa separa en números de 2 cifras, los
+suma e imprime el resultado. En caso de no poderse dividir exactamente en números de n
+cifras se colocan ceros a la izquierda del primer número.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
